28ce04a4abfdd9ea95fac7ce68307f02f4224775
liulinboyi <814921718@qq.com>
2021-11-01 21:00:02 +0800

    feat: add expressions

---
+++ src/expressions.rs
@@ -0,0 +1,142 @@
+// 流程控制
+// Rust是一个基于表达式的语言
+// 表达式总是会返回一个值，而语句不会
+// 1 + 1; // 表达式
+// let a = 1; // 语句
+// Rust大多数代码都会有一个返回值，除了一下语法：
+
+// 1.变量声明
+// 2.模块声明
+// 3.函数声明
+// 4.结构体声明
+// 5.枚举声明
+// ...
+// if else 也是表达式
+pub fn my_expressions() {
+    println!("-------------expressions------------------");
+    let flag = true;
+    // if else 非常像三元表达式
+    let a = if flag { 10 } else { 20 };
+    println!("a = {}", a);
+
+    // loop
+    let mut s = 0;
+    let mut n = 10;
+
+    // 从0加到10
+    let c = loop {
+        if n < 0 {
+            break s;
+        }
+        s += n;
+        n -= 1;
+    };
+
+    println!("c = {}", c);
+
+    println!("-------------if else------------------");
+    // 必须保证if else所有分支的返回值类型相同
+
+    let n = 10;
+    let res = if n < 0 {
+        "负数"
+    } else if n > 0 {
+        "正数"
+    } else {
+        "0"
+    };
+
+    println!("n是{}", res);
+
+    println!("-------------loop------------------");
+
+    // loop表示无限循环
+    // 相当于别的语言的while(true)
+    // break 退出循环
+    // continue 跳过其余代码并开始下一次循环
+
+    // 计算 1 + 2 + ... + 100
+
+    let mut start = 1;
+    let end = 100;
+    let mut sum = 0;
+    sum = loop {
+        sum += start;
+        start += 1;
+        if start > end {
+            break sum;
+        }
+    };
+
+    println!("1 + 2 + ... + 100 = {}", sum);
+
+    println!("-------------while------------------");
+
+    // 本质上就是一个带循环条件的loop
+    // 条件为假时结束循环
+
+    let mut s = 1;
+    while s <= 100 {
+        if s % 15 == 0 {
+            println!("FizzBuzz");
+        } else if s % 3 == 0 {
+            println!("Fizz");
+        } else if s % 5 == 0 {
+            println!("Buzz");
+        } else {
+            // println!("{}",s);
+        }
+        s += 1;
+    }
+
+    println!("-------------for range------------------");
+    // for ... in ...可以遍历一个迭代器
+    // 有多种方法创建一个迭代器
+    // 1.a..b 创建一个包含a不包含b，步长为1的迭代器
+    // 2.a..=b 创建一个包含且包含b，步长为1的迭代器
+
+    for i in 0..5 {
+        println!("0..5 {}", i);
+    }
+
+    for i in 0..=5 {
+        println!("0..=5 {}", i);
+    }
+
+    // 遍历数组，前提是将数组转换成迭代器 可以使用数组的 .iter() 或 .iter_mut() 方法实现 区别是后者是可变的
+
+    let mut color = [String::from("红"), String::from("蓝"), String::from("绿")];
+    for item in color.iter() {
+        println!("{}", item);
+    }
+
+    for item in color.iter_mut() {
+        item.push('0');
+        println!("{}", item);
+    }
+
+    println!("-------------match------------------");
+
+    enum Alphabet {
+        A,
+        B,
+    }
+    let letter = Alphabet::A;
+    match letter {
+        Alphabet::A => {
+            println!("A");
+        }
+        Alphabet::B => {
+            println!("B");
+        }
+    }
+
+    let op_code: u8 = 30;
+
+    match op_code {
+        30 => {
+            println!("It is ok!");
+        }
+        _=> {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ pub mod my_slice;
 pub mod my_struct;
 pub mod my_enum;
 pub mod my_type_conversion;
+pub mod expressions;
 
 const COUNT: i32 = 10;
 
@@ -29,6 +30,7 @@ fn main() {
     test_my_struct();
     test_my_enum();
     test_my_type_conversion();
+    test_my_expressions();
 }
 
 fn test_main() {
@@ -88,4 +90,8 @@ fn test_my_enum() {
 
 fn test_my_type_conversion() {
     my_type_conversion::my_type_conversion();
+}
+
+fn test_my_expressions() {
+    expressions::my_expressions();
 }
